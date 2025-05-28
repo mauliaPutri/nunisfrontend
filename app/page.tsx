60ca23ad4104101804f92f7eaf42f239378eb613
LAPTOP-QEJT3KSG\HP <rahmadwimaulia.16@gmail.com>
2025-05-28 23:14:31 +0700

    [update]

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import Login from './login/login';
 
 export default function LoginPage() {
   return (
     <>
+    <Login />
     </>
   )
 }
